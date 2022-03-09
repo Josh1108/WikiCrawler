1317eb63dfbd992e74c446ebb4cf9826bd01956b
Jushaan Kalra <josh@Jushaans-MacBook-Air.local>
2022-03-10 03:54:38 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,38 @@
+module crawler
+
+go 1.17
+
+require (
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.9 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/pebble v0.0.0-20220308174854-6cebaf524d7a // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type WebPage struct {
+	Url  string `json:"url"`
+	Text string `json:"title"`
+}
+
+func add_to_db(key string, value string) {
+
+	db, err := leveldb.OpenFile("./data.db", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = db.Put([]byte(key), []byte(value), nil)
+	fmt.Println(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+}
+
+func return_url(text string) {
+	db, err := leveldb.OpenFile("./data.db", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	iter := db.NewIterator(nil, nil)
+	for iter.Next() {
+		key := string(iter.Key())
+		fmt.Println(key)
+		value := string(iter.Value())
+		if strings.Contains(value, text) {
+			fmt.Println(key)
+		}
+	}
+	iter.Release()
+	_ = iter.Error()
+	defer db.Close()
+}
+func crawler() {
+
+	var webpage WebPage
+	c := colly.NewCollector(
+		colly.AllowedDomains("wikipedia.org", "en.wikipedia.org", "https://en.wikipedia.org/"),
+		// colly.MaxDepth(1),
+		colly.Async(true),
+	)
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+
+		webpage.Text = e.ChildText("p")
+		webpage.Url = e.Attr("href")
+		println("here", webpage.Text)
+		add_to_db(webpage.Url, webpage.Text)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// link := e.Attr("href")
+		// c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		RandomDelay: 1 * time.Second,
+	})
+
+	c.OnRequest(func(r *colly.Request) { // url
+
+		fmt.Println("Crawl on Page", r.URL.String())
+	})
+	url := "https://en.wikipedia.org/wiki/2022_Peshawar_mosque_attack"
+
+	c.Visit(url)
+	c.Wait()
+
+}
+
+func main() {
+
+	crawler()
+	fmt.Println("==========Crawled pages ==========")
+	fmt.Println("==========links where text is found ==========")
+
+	return_url("act")
+
+}
